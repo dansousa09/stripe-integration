62e796f17381b78e2182c9e1627e8bd8d05dc212
Danilove <dansousa09@gmail.com>
2024-09-19 12:19:29 -0300

    refactor(redis): update Redis configuration to use environment variables

--- src/infra/cache/redis.ts
+++ src/infra/cache/redis.ts
@@ -1,10 +1,11 @@
 import Redis from 'ioredis';
+import { env } from '../../schemas/env.schema';
 
 const redis = new Redis({
-  host: 'localhost',
-  port: 6379,
+  host: env.REDIS_HOST,
+  port: Number(env.REDIS_PORT),
 }).once('connect', () => {
-  console.log(' 🚀 Redis connected successfully on port 6379');
+  console.log(` 🚀 Redis connected successfully on port ${env.REDIS_PORT}`);
 });
 
 export default redis;
