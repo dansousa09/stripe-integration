10dcb5d8b1b661adaa46eaa442d93ad3e2ed7a9b
Danilove <dansousa09@gmail.com>
2024-09-19 12:11:00 -0300

    feat(env): add env.schema.ts for validating environment variables

---
+++ src/schemas/env.schema.ts
@@ -0,0 +1,35 @@
+import { z } from 'zod';
+
+const envSchema = z.object({
+  NODE_ENV: z.string().default('development'),
+  REDIS_HOST: z
+    .string({
+      description: 'The host of the Redis server',
+    })
+    .default('localhost'),
+  REDIS_PORT: z
+    .string({
+      description: 'The port of the Redis server',
+    })
+    .default('6379'),
+  MONGODB_URL: z
+    .string({
+      description: 'The URL of the MongoDB server',
+    })
+    .url()
+    .default('mongodb://localhost:27017'),
+});
+
+const envParsed = envSchema.safeParse(process.env);
+
+if (!envParsed.success) {
+  throw new Error(
+    `❌ Invalid environment variables: ${JSON.stringify(
+      envParsed.error.format(),
+      null,
+      4,
+    )}`,
+  );
+}
+
+export const env = envParsed.data;
