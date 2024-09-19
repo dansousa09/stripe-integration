21c4cf7ecd20008d4c259d99869b58e9b4937e78
Danilove <dansousa09@gmail.com>
2024-09-19 12:19:42 -0300

    refactor(database): update MongoDB configuration to use environment variables

--- src/infra/database/mongodb.ts
+++ src/infra/database/mongodb.ts
@@ -1,23 +1,32 @@
 import { MongoClient } from 'mongodb';
+import { env } from '../../schemas/env.schema';
 
-const url = 'mongodb://localhost:27017';
+const url = env.MONGODB_URL;
 
 const client = new MongoClient(url);
 
 // biome-ignore lint/suspicious/noExplicitAny: <explanation>
 async function mongo(operation: (client: MongoClient) => Promise<any>) {
+  let success = false;
   try {
     await client.connect();
-    console.log('🚀 Mongodb connected successfully on port 27017');
+    console.info(
+      ' 🧊 Mongodb connected successfully and is ready to perform the operation',
+    );
 
     const result = await operation(client);
+    success = true;
     return result;
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
     throw error;
   } finally {
     await client.close();
-    console.log('🚀 Mongodb connection closed');
+    if (success) {
+      console.info(' 🧊 The Mongodb operation was performed successfully');
+    } else {
+      console.error(' ❌ Mongodb disconnected with errors');
+    }
   }
 }
 
